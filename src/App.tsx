7ea2462cffa9341875e3773fced62e3682651866
noopur-phadkar <np7541@rit.edu>
2024-03-12 03:13:08 -0400

    adding project files after fixing bugs

---
+++ src/App.tsx
@@ -0,0 +1,104 @@
+import { Fragment, useCallback, useEffect, useMemo, useState } from "react"
+import { InputSelect } from "./components/InputSelect"
+import { Instructions } from "./components/Instructions"
+import { Transactions } from "./components/Transactions"
+import { useEmployees } from "./hooks/useEmployees"
+import { usePaginatedTransactions } from "./hooks/usePaginatedTransactions"
+import { useTransactionsByEmployee } from "./hooks/useTransactionsByEmployee"
+import { EMPTY_EMPLOYEE } from "./utils/constants"
+import { Employee } from "./utils/types"
+
+export function App() {
+  const { data: employees, ...employeeUtils } = useEmployees()
+  const { data: paginatedTransactions, ...paginatedTransactionsUtils } = usePaginatedTransactions()
+  const { data: transactionsByEmployee, ...transactionsByEmployeeUtils } = useTransactionsByEmployee()
+  const [isLoading, setIsLoading] = useState(false)
+  // Add a state to track the selected employee
+  const [selectedEmployeeId, setSelectedEmployeeId] = useState<string | null>(null);
+
+
+  const transactions = useMemo(
+    () => paginatedTransactions?.data ?? transactionsByEmployee ?? null,
+    [paginatedTransactions, transactionsByEmployee]
+  )
+
+  const loadAllTransactions = useCallback(async () => {
+    setIsLoading(true)
+    transactionsByEmployeeUtils.invalidateData()
+
+    await employeeUtils.fetchAll()
+    setIsLoading(false)
+
+    await paginatedTransactionsUtils.fetchAll()
+
+  }, [employeeUtils, paginatedTransactionsUtils, transactionsByEmployeeUtils])
+
+  const loadTransactionsByEmployee = useCallback(
+    async (employeeId: string | null) => {
+      paginatedTransactionsUtils.invalidateData();
+      if (employeeId) {
+        await transactionsByEmployeeUtils.fetchById(employeeId);
+      } else {
+        // If employeeId is null, fetch all data
+        await employeeUtils.fetchAll();
+        await paginatedTransactionsUtils.fetchAll();
+      }
+    },
+    [employeeUtils, paginatedTransactionsUtils, transactionsByEmployeeUtils]
+  );
+  
+
+  useEffect(() => {
+    if (employees === null && !employeeUtils.loading) {
+      loadAllTransactions()
+    }
+  }, [employeeUtils.loading, employees, loadAllTransactions])
+
+  return (
+    <Fragment>
+      <main className="MainContainer">
+        <Instructions />
+
+        <hr className="RampBreak--l" />
+
+        <InputSelect<Employee>
+          isLoading={isLoading}
+          defaultValue={EMPTY_EMPLOYEE}
+          items={employees === null ? [] : [EMPTY_EMPLOYEE, ...employees]}
+          label="Filter by employee"
+          loadingLabel="Loading employees"
+          parseItem={(item) => ({
+            value: item.id,
+            label: `${item.firstName} ${item.lastName}`,
+          })}
+          onChange={async (newValue) => {
+            setSelectedEmployeeId(newValue ? newValue.id : null);
+            if (newValue === null) {
+              return
+            }
+
+            await loadTransactionsByEmployee(newValue.id)
+          }}
+        />
+
+        <div className="RampBreak--l" />
+
+        <div className="RampGrid">
+          <Transactions transactions={transactions} />
+
+          {transactions !== null && selectedEmployeeId === null &&  (
+            <button
+              className="RampButton"
+              disabled={paginatedTransactionsUtils.loading}
+              onClick={async () => {
+                await loadAllTransactions()
+              }}
+            >
+              View More
+            </button>
+          )}
+        </div>
+      </main>
+    </Fragment>
+  )
+}
